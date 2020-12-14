c5090b52a6443ef323b6dd887b559964784501df
OjanayR <69225985+OjanayR@users.noreply.github.com>
2020-12-14 13:43:30 -0500

    add README.md

--- src/pages/InsideNYCPage.js
+++ src/pages/InsideNYCPage.js
@@ -1,65 +1,57 @@
 
-import React, { useState } from "react";
-import { NYCinfoData}  from '../NYCinfoData'
+import React, { useState } from 'react'
 
-
-/*import "./App.scss";*/
-
-
-export default class InsideNYCPage extends useState {
-    state={
-        NYCinfo: NYCinfoData
-    };
-    render() {
-        const { city, img, name, info } = this.props.nycinfodata;
-
-    {/*
-        return <article className="InsideNYC">
-
-    <div className ="img-container">
-    <img 
-    src={} 
-    alt=""
-    /> 
-    <span className="close-btn">
-        <i className="fas fa-window-close" />
-    </span>  
-    </div>
-    <div className="NYCinfo">
-        <h3>{city}</h3>
-        <h4>{name}</h4>
-        <h5>
-            Info{""}
-            <span>
-                <i className="fas fa-caret-square-down" />
-        </span>
-        </h5>
-        <p>{info}</p>
-    </div>
-    </article>;
-    }  
-}
-*/}
-
-
-
-{/*
-return (
-<section className="nycinfodata">
-{NYCinfo.map(NYCinfo => {
-    return <InsideNYCPage key={NYCinfoData.id} NYCinfoData = {NYCinfoData} />;
-})}
-</section>
-);
-*/}
-
-
-{/*
-export default function DashboardPage(props) {
-    return (
-        <main className="Page">
-            <h1>Inside NYC</h1>
-        </main>
+const NYCinfo =({ id, image, name, info, removeNYCinfo}) => {
+    const [readMore, setReadMore] = useState(false);
+    return(
+        <article className = "single-nycinfo">
+            <img src={image} alt={name} />
+            <footer>
+                <div className="nycinfo-info">
+                    <h4>{name}</h4>
+                </div>
+                <p>
+                    {readMore ? info: `${info.substring(0, 200)}...`}
+                    <button onClick={() => setReadMore(!readMore)}>
+                        {readMore ? 'show less' : ' read more'}
+                    </button>
+                </p>
+                <button className ="delet-btn" onClick={() => removeNYCinfo(id)}>
+                    No, thank you.
+                </button>
+            </footer>
+        </article>
     );
 };
-*/}
+
+ export default NYCinfo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
