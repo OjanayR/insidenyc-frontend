7435392543198605346998648c2e7440d0d26737
OjanayR <69225985+OjanayR@users.noreply.github.com>
2020-12-13 21:54:38 -0500

    created nycinfo data file

---
+++ src/components/Header/NYCinfo/NYCinfoData.js
@@ -0,0 +1,153 @@
+export const NYCinfoData = [
+    {
+        id: 1,
+        city: "Brooklyn, NY",
+        img: "./img/BarboncinoPizzaBar.png",
+        name: "Barboncino Pizza Bar",
+        info: "Lively eatery serving Neapolitan brick-oven pizzas plus salads, creative cocktails & more."
+    },
+    {
+        id: 2,
+        city: "China Town, NY",
+        img: "./img/MelissasDumplings.png",
+        name: "Vanessa/s Dumplings",
+        info: "Handmade dumplings, steamed buns & other Asian faves, plus bubble teas."
+    },
+    {
+        id: 3,
+        city: "Korea Town, NY",
+        img: "./img/JongroBBQ.png",
+        name: "Jongro BBQ",
+        info: "Korean BBQ & other traditional dishes served in a casual, open-timbered space with in-table grills."
+    },
+    {
+        id: 4,
+        city: "China Town, NY",
+        img: "./img/NomWahTeaParlor.png",
+        name: "Nom Wah Tea Parlor",
+        info: "Vintage dim sum parlor dating back to 1920 with a drink menu that includes tea, beer & wine."
+    },
+    {
+        id: 5, 
+        city: "Bronx, NY",
+        img: "./img/Patricias.png",
+        name: "Patricia/s of Morris Park",
+        info: "Cozy, brick-walled Italian eatery offering homestyle pastas, wood-fired pizzas & a large wine list."
+    },
+    {
+        id: 6,
+        city: "City Island, Bronx, NY",
+        img: "./img/SammysFishBox.png",
+        name: "Sammy/s Fish Box",
+        info: "Bustling, spacious family seafood spot, with big portions, booth seating & stained-glass decor."
+    },
+    {
+        id: 7,
+        city: "New York, NY",
+        img: "./img/SouvlakiGR.png",
+        name: "SouvlakiGR",
+        info: "Authentic Greek Souvlaki in the city."
+    },
+    {
+        id: 8,
+        city: "New York, NY",
+        img: "./img/TheGrayMare.png",
+        name: "The Gray Mare",
+        info: "Draft beer, cocktails & American bar plates in a laid-back pub setting with rustic decor."
+    },
+    {
+        id: 9,
+        city: "Brooklyn, NY",
+        img: "./img/ZanmiNYC.png",
+        name: "Zanmi NYC", 
+        info: "Zanmi is hub for Haitian-American cuisine and most importantly community."
+    },
+    {
+        id: 10,
+        city:"Bronx, NY",
+        img: "./img/BronxZoo.png",
+        name: "Bronx Zoo",
+        info: "One of the largest zoos in the United States by area, and is the largest metropolitan zoo in the United States by area, comprising 265 acres of park lands and naturalistic habitats separated by the Bronx River."
+    },
+    {
+        id: 11,
+        city: "Bronx, NY",
+        img: "./img/BronxMuseumOfTheArts.png",
+        name: "The Bronx Museum of the Arts",
+        info: "The Bronx Museum of the Arts is an internationally recognized cultural destination that presents innovative contemporary art exhibitions and educational programs for Bronx residents and beyond."
+    },
+    {
+        id: 12,
+        city: "Bronx, NY",
+        img: "./img/BxBotanicalGarden.png",
+        name: "New York Botanical Garden, aka The Bronx Botanical Garden",
+        info: "Established in 1891, The New York Botanical Garden (NYBG) is distinguished by the beauty of its landscape, collections, and gardens, and the scope and excellence of its programs in horticulture, education, and science."
+    },
+    {
+        id: 13,
+        city: "Bronx, NY",
+        img: "./img/OrchardBeach.png",
+        name: "Orchard Beach",
+        info: "Orchard Beach is a public beach in the Bronx, New York City. The beach is part of Pelham Bay Park and is situated on the western end of Long Island Sound."
+    },
+    {
+        id: 14,
+        city: "Bronx, NY",
+        img: "./img/VanCortlandtPark.png",
+        name: "Van Courtlandt Park",
+        info: "Van Cortlandt Park is a 1,146-acre (464 ha) park located in the borough of the Bronx in New York City. Van Cortlandt Park's sports facilities include golf courses and several miles of paths for running, as well as facilities for baseball, basketball, cricket, cross-country running, football, horseback riding, lacrosse, rugby, soccer, softball, swimming, tennis and track and field. The park also contains five major hiking trails and other walking trails."
+    },
+    {
+        id: 15,
+        city: "Brooklyn, NY",
+        img: "./img/BrooklynPromenade.png",
+        name: "Brooklyn Promenade",
+        info: "The Brooklyn Heights Promenade, also called the Esplanade, is a 1,826-foot -long platform and pedestrian walkway cantilevered over the Brooklyn-Queens Expressway in Brooklyn Heights, Brooklyn."
+    },
+    {
+        id: 16,
+        city: "Brooklyn, NY",
+        img: "./img/ProspectPark.png",
+        name: "Prospect Park",
+        info: "An urban park in Brooklyn, New York City. The park is situated between the neighborhoods of Park Slope, Prospect Heights, Prospect Lefferts Gardens, Flatbush, and Windsor Terrace, and is adjacent to the Brooklyn Museum, Grand Army Plaza, and the Brooklyn Botanic Garden."
+    },
+    {
+        id: 17, 
+        city: "New York, NY",
+        img: "./img/ChinaTown2.png",
+        name: "Chinatown",
+        info: "Vibrant neighborhood that draws foodies and tourists to its many Chinese and Southeast Asian restaurants for dumplings, pork buns and hand-pulled noodles. The busy sidewalks are packed with souvenir stores, bubble tea shops, and markets selling everything from fresh and dried fish to herbs and spices. "
+    },
+    {
+        id: 18,
+        city: "New York, NY",
+        img: "./img/HightLine2.png",
+        name: "The High Line",
+        info: "The High Line is a 1.45-mile-long elevated linear park, greenway and rail trail created on a former New York Central Railroad spur on the west side of Manhattan in New York City."
+    },
+    {
+        id: 19,
+        city: "New York, NY",
+        img: "./img/HudsonRiverPark.png",
+        name: "Hudson River Park",
+        info: "Hudson River Park is a waterside park on the North River (Hudson River) that extends from 59th Street south to Battery Park in the New York City borough of Manhattan."
+    },
+    {
+        id: 20,
+        city: "New York, NY",
+        img: "./img/GovIsland.png",
+        name: "Governors Island",
+        info: "Governors Island is a 172-acre island (!) in New York Harbor, within the New York City borough of Manhattan."
+    }
+    
+
+
+
+
+
+
+
+
+
+
+]
