efe881a4b902daf16227f887c5ac3a1470da2ed2
OjanayR <69225985+OjanayR@users.noreply.github.com>
2020-12-14 21:39:06 -0500

    adds content

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Inside NYC
-### Inside NYC is an app tourist can use when traveling to New York City. Inside NYC provides tourists the opportunity to visit NYC restaurants and attractions only known to most New Yorkers. 
+### 'Inside NYC' is an app tourist can use when traveling in New York City. 'Inside NYC' provides tourists the opportunity to visit NYC restaurants and attractions only known to New Yorkers. 
 ### On the app, you will find the name of the restaurant or attraction, their location (borough), an image and a brief summary.
 
 
--- src/pages/InsideNYCPage.js
+++ src/pages/InsideNYCPage.js
@@ -2,10 +2,28 @@ import BarboncinoPizzaBar from "./../image/BarboncinoPizzaBar.png"
 import VanessaDumplings from "./../image/VanessasDumplings.png"
 import JongroBBQ from "./../image/JongroBBQ.png"
 import NomWahTeaParlor from "./../image/NomWahTeaParlor.png"
+import PatriciaOnMorris from "./../image/Patricias.png"
+import SammysFishBox from './../image/SammysFishBox.jpg'
+import SouvlakiGR from "./../image/SouvlakiGR.png"
+import TheGrayMare from "./../image/TheGrayMare.png"
+import ZanmiNYC from "./../image/ZanmiNYC.png"
+import BronxMuseum from "./../image/BronxMuseum2.jpg"
+import BronxZoo from "./../image/BronxZoo.png"
+import BxBontanicalGarden from "./../image/BxBotanicalGarden.png"
+import OrchardBeach from "./../image/OrchardBeach.png"
+import VanCortlandtPark from "./../image/VanCortlandtPark.png"
+import BrooklynPromenade from "./../image/BrooklynPromenade.png"
+import ProspectPark from "./../image/ProspectPark.png"
+import GovIsland from "./../image/GovIsland.png"
+import HighLine from "./../image/HightLine2.png"
+import Chinatown from "./../image/ChinaTown2.png"
+import DitmasPark from "./../image/DitmasPark.jpg"
+
 
 export default function DashboardPage(props) {
     return (
         <main className="Page">
+            <h1><strong> /r e s t a u r a n t s/ </strong></h1>
             <h1>barboncino pizza bar</h1>
             <img src= {BarboncinoPizzaBar} alt="pizza" />
             <p>
@@ -33,14 +51,172 @@ export default function DashboardPage(props) {
         </ul>
             </p>
 
-            <h1>nom wah tea parlor</h1>
+           <h1>nom wah tea parlor</h1>
             <img src= {NomWahTeaParlor} alt="dim sum" />
             <p>
         <ul>
         <li>city: Chinatown, NY</li>
         <li>info: Vintage dim sum parlor dating back to 1920 with a drink menu that includes tea, beer & wine.</li>
         </ul>
+            </p> 
+
+            <h1>particia's on morris</h1>
+            <img src= {PatriciaOnMorris} alt="italian" />
+            <p>
+        <ul>
+        <li>city: Bronx, NY</li>
+        <li>info: Cozy, brick-walled Italian eatery offering homestyle pastas, wood-fired pizzas & a large wine list.</li>
+        </ul>
+            </p> 
+
+            <h1>sammy's fish box</h1>
+            <img src= {SammysFishBox} alt="seafood" />
+            <p>
+        <ul>
+        <li>city: City Island, Bronx, NY</li>
+        <li>info: Bustling, spacious family seafood spot, with big portions, booth seating & stained-glass decor.</li>
+        </ul>
+            </p> 
+
+            <h1>souvlakiGR</h1>
+            <img src= {SouvlakiGR} alt="greek" />
+            <p>
+        <ul>
+        <li>city: New York, NY</li>
+        <li>info: Authentic Greek Souvlaki in the city.</li>
+        </ul>
+            </p> 
+
+            <h1>the gray mare</h1>
+            <img src= {TheGrayMare} alt="american" />
+            <p>
+        <ul>
+        <li>city: New York, NY</li>
+        <li>info: Draft beer, cocktails & American bar plates in a laid-back pub setting with rustic decor.</li>
+        </ul>
+            </p> 
+
+            <h1>zanmi nyc</h1>
+            <img src= {ZanmiNYC} alt="Haitian" />
+            <p>
+        <ul>
+        <li>city: Brooklyn, NY</li>
+        <li>info: Zanmi is hub for Haitian-American cuisine and most importantly, community.</li>
+        </ul>
+            </p> 
+
+ 
+            <h1><strong> /a t t r a c t i o n s/ </strong></h1>
+            <h1>bronx museum of the arts </h1>
+            <img src= {BronxMuseum} alt="art" />
+            <p>
+        <ul>
+        <li>city: Bronx, NY</li>
+        <li>info: The Bronx Museum of the Arts is an internationally recognized cultural destination that presents innovative contemporary art exhibitions and educational programs for Bronx residents and beyonds.</li>
+        </ul>
             </p>
+
+            <h1>bronx zoo </h1>
+            <img src= {BronxZoo} alt="art" />
+            <p>
+        <ul>
+        <li>city: Bronx, NY</li>
+        <li>info: One of the largest zoos in the United States by area, and is the largest metropolitan zoo in the United States by area, comprising 265 acres of park lands and naturalistic habitats separated by the Bronx River.</li>
+        </ul>
+            </p>
+            
+            <h1>bronx botantical garden </h1>
+            <img src= {BxBontanicalGarden} alt="art" />
+            <p>
+        <ul>
+        <li>city: Bronx, NY</li>
+        <li>info: Established in 1891, The New York Botanical Garden (NYBG) is distinguished by the beauty of its landscape, collections, and gardens, and the scope and excellence of its programs in horticulture, education, and science.</li>
+        </ul>
+            </p>
+
+            <h1>orchard beach </h1>
+            <img src= {OrchardBeach} alt="art" />
+            <p>
+        <ul>
+        <li>city: Bronx, NY</li>
+        <li>info: Orchard Beach is a public beach in the Bronx, New York City. The beach is part of Pelham Bay Park and is situated on the western end of Long Island Sound.</li>
+        </ul>
+            </p>
+
+            <h1>van cortlandt park</h1>
+            <img src= {VanCortlandtPark} alt="park" />
+            <p>
+        <ul>
+        <li>city: Bronx, NY</li>
+        <li>info: Van Cortlandt Park is a 1,146-acre park located in the borough of the Bronx in New York City. Van Cortlandt Park's sports facilities include golf courses and several miles of paths for running, as well as facilities for baseball, basketball, cricket, cross-country running, football, horseback riding, lacrosse, rugby, soccer, softball, swimming, tennis and track and field. The park also contains five major hiking trails and other walking trails</li>
+        </ul>
+            </p>
+
+            <h1>brooklyn promenade</h1>
+            <img src= {BrooklynPromenade} alt="park" />
+            <p>
+        <ul>
+        <li>city: Brooklyn, NY</li>
+        <li>info: The Brooklyn Heights Promenade, also called the Esplanade, is a 1,826-foot -long platform and pedestrian walkway cantilevered over the Brooklyn-Queens Expressway in Brooklyn Heights, Brooklyn.</li>
+        </ul>
+            </p>
+
+            <h1>prospect park</h1>
+            <img src= {ProspectPark} alt="park" />
+            <p>
+        <ul>
+        <li>city: Brooklyn, NY</li>
+        <li>info: An urban park in Brooklyn, New York City. The park is situated between the neighborhoods of Park Slope, Prospect Heights, Prospect Lefferts Gardens, Flatbush, and Windsor Terrace, and is adjacent to the Brooklyn Museum, Grand Army Plaza, and the Brooklyn Botanic Garden.</li>
+        </ul>
+            </p>
+
+            <h1>governors island</h1>
+            <img src= {GovIsland} alt="park" />
+            <p>
+        <ul>
+        <li>city: New York, NY</li>
+        <li>info: Governors Island is a 172-acre island (!) in New York Harbor, within the New York City borough of Manhattan</li>
+        </ul>
+            </p>
+
+            <h1>high line</h1>
+            <img src= {HighLine} alt="park" />
+            <p>
+        <ul>
+        <li>city: New York, NY</li>
+        <li>info: The High Line is a 1.45-mile-long elevated linear park, greenway and rail trail created on a former New York Central Railroad spur on the west side of Manhattan in New York City.</li>
+        </ul>
+            </p>
+
+            <h1>chinatown</h1>
+            <img src= {Chinatown} alt="park" />
+            <p>
+        <ul>
+        <li>city: New York, NY</li>
+        <li>info: Vibrant neighborhood that draws foodies and tourists to its many Chinese and Southeast Asian restaurants for dumplings, pork buns and hand-pulled noodles. The busy sidewalks are packed with souvenir stores, bubble tea shops, and markets selling everything from fresh and dried fish to herbs and spices.</li>
+        </ul>
+            </p>
+
+            <h1>ditmas park</h1>
+            <img src= {DitmasPark} alt="park" />
+            <p>
+        <ul>
+        <li>city: Brooklyn, NY</li>
+        <li>info: Located in the Flatbush neighborhood in the borough of Brooklyn, founded in 1651 by Dutch colonists.</li>
+        </ul>
+            </p>
+
+
+
+
+
+
+
+
+
+
+
+
 
             
 
